0f26fc8693d3b4adc49a156bbdddce1a3c9a7f7f
Jørgen Vatle <Jorgen@WcAServices.net>
2018-12-17 00:44:39 +0200

    Add leading line break to gimme-ip success message.

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const clipboardy = require('clipboardy');
 
 request.get("https://canihazip.com/s", function(error, response, body) {
     clipboardy.writeSync(body);
-    console.log(`Copied IP address to clipboard: ${body}`);
+    console.log(`\nCopied IP address to clipboard: ${body}`);
 });
